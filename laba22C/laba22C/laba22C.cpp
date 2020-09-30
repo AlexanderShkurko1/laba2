8a8fd06008796b39f552c95df5fc1ca4281c6009
AlexanderShkurko1 <71352620+AlexanderShkurko1@users.noreply.github.com>
2020-09-30 12:56:35 +0300

    Add files via upload

---
+++ laba22C/laba22C/laba22C.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+
+	int arr[30][30];                      //Объявление переменных
+	int p = 1;
+	int n;
+
+	printf("Введите n: ");
+	scanf_s("%d", &n);                          //Ввод размера матрицы
+
+	for (int i = 0; i < n; i++)           //Выполняется для каждой строки
+	{
+		for (int j = 0; j < n; j++)       //Для каждого элемента строки 
+			scanf_s("%d", &arr[i][j]);            //Присваивание элементу с индексом i j вводимого значения 
+	}
+
+	// Находим произведение элементов второй строки                                            
+	for (int j = 0; j < n; j++) {
+		p *= arr[1][j];
+	}
+	printf("\nПроизв. элементов 2 строки: %d", p);
+	getchar();
+}
+
+
